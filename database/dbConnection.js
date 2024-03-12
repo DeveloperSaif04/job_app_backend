8b31edd0dc9f331789e67feeafb8af41b78b70de
DeveloperSaif04 <saif.coder04@gmail.com>
2024-03-12 16:20:58 +0530

    url updated

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 
-console.log(process.env)
 
 export const dbConnection = () => {
+
+  console.log("sdj",process.env.MONGO_URL+'/job')
   mongoose
-    .connect(process.env.MONGO_URL, {
-      dbName: "JOB_APPLICATION",
-    })
+    .connect(process.env.MONGO_URL+'/job')
     .then(() => {
       console.log("Connected to database.");
     })
-    .catch((err) => {
-      console.log(`Some Error occured. ${err}`);
-    });
+   
 };
