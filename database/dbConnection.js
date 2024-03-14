ad517f75ed3331ac056d2adf2aab6e513cb02af9
DeveloperSaif04 <saif.coder04@gmail.com>
2024-03-14 23:22:09 +0530

    hard code val

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
 
-  console.log("sdj",process.env.MONGO_URL+'/job')
+  // console.log("sdj",process.env.MONGO_URL+'/job')
   mongoose
-    .connect(process.env.MONGO_URL)
+    .connect("mongodb+srv://saifcoder04:EKAKFQT8pLL6JJjT@cluster0.dcf8is1.mongodb.net/")
     .then(() => {
       console.log("Connected to database.");
     })
--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import app from "./app.js";
 import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
-  api_key: process.env.CLOUDINARY_CLIENT_API,
-  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+  // cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  // api_key: process.env.CLOUDINARY_CLIENT_API,
+  // api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+
+
+  CLOUDINARY_CLIENT_NAME:"dy8soaigi",
+CLOUDINARY_CLIENT_API:"128465349387969",
+CLOUDINARY_CLIENT_SECRET:"UrbkyNw49F8GsYjw5awwdvM9rfk",
+
 });
 
 app.listen(process.env.PORT, () => {
